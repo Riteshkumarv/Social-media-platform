506f0a7bc49f54b3183704843134b88219f7a3a7
Riteshkumarv <Ritesh74461@gmail.com>
2025-09-29 12:21:03 +0530

    [update]

--- Back-end/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ Back-end/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,22 +1,20 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(title = "My API", version = "1.0", description = "Spring Boot JWT API"),
-        security = {@SecurityRequirement(name = "bearerAuth")}
-)
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://backend-production-6085.up.railway.app")
+                ));
+    }
 }
